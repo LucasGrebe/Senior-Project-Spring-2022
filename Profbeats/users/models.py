1ce4125c9147fb2d91f7899ae422f80ab99d4d7d
Vincent Sinigaglio <siniga47@students.rowan.edu>
2022-04-06 16:43:44 -0400

    Customuser app added to main branch.

---
+++ Profbeats/users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+# Create models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
