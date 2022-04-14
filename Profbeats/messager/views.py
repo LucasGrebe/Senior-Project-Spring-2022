e3496b83d617a42db7f2d7b25906fa27b698e380
Devyn Melendez <melend53@students.rowan.edu>
2022-04-13 20:54:44 -0400

    Messager pages now redirect to the login page if the user is not logged in, and also pass a Django message that alerts the user that they need to log in to use that page. Django message-displaying functionality added to the login page. Also, fixed some typos.

--- Profbeats/messager/views.py
+++ Profbeats/messager/views.py
@@ -5,7 +5,6 @@ from .forms import *
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages as djangomessages
 
 auth_manager = SpotifyClientCredentials(client_id='96652f337059433b8015a2fc3b8922fa', client_secret='bc7324bf495b401abd4018f2a5404054')
@@ -14,14 +13,12 @@ sp = spotipy.Spotify(auth='BQBudGJkAEas3zrtctXFBRveiTFae3AIqOC', auth_manager=au
 def messages(request):
     context = {}
 
-    # temporary login code
-    """
-    logout(request)
+    # You need to be logged in to send/read messages.
+    # Redirect to the login page if the user is not logged in.
     if not request.user.is_authenticated:
-        user = authenticate(request, username="admin", password="admin")
-        login(request, user)
-    """
-
+        djangomessages.warning(request, ("You need to be logged in to read and send messages."))
+        return redirect('login')
+    
     context['inbox'] = Message.objects.filter(recipient = request.user.get_username()).order_by('-created_at')
     if not context['inbox']:
         djangomessages.info(request, ("Your inbox is empty. Send someone a message to strike up a conversation!"))
@@ -36,6 +33,12 @@ def deletemessage(request, message_id):
 
 def writemessage(request):
     context = {}
+
+    # You need to be logged in to write messages.
+    # Redirect to the login page if the user is not logged in.
+    if not request.user.is_authenticated:
+        djangomessages.warning(request, ("You need to be logged in to read and send messages."))
+        return redirect('login')
 
     if request.method == 'POST':
         form = MessageForm(request.POST)
