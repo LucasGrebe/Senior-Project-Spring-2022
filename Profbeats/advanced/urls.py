61f535dca41d63158add942ee7026b8ab110741e
NickBornkamp <bornka58@students.rowan.edu>
2022-04-20 15:51:09 -0400

    Attempted to integrate the advanced search.

--- Profbeats/advanced/urls.py
+++ Profbeats/advanced/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 
-#app_name = 'recommender'
+#app_name = 'advanced'
 
 urlpatterns = [
     
-    path('', views.advanced_get, name='advanced'),
-    path('/results/', views.advanced_post, name='results'),
+    path('advanced_search/', views.advanced_get, name='advsearch'),
+    path('advanced_results/', views.advanced_post, name='results'),
 
 ]
  
